93f1f812727cc0bf6030bd1c134ac8da5f42c3a5
nirdudik6 <68653241+nirdudik6@users.noreply.github.com>
2020-11-12 08:30:32 +0200

    Add files via upload

---
+++ instagram_login.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from time import sleep
+from creditentials import username, password
+
+
+
+
+class InstaUnfollowers:
+    def __init__(self, username, password):
+        self.driver = webdriver.Chrome("C:/Users/turtleman/Desktop/chromedriver")
+        self.driver.get("https://instagram.com")
+        sleep(2)
+        # instagram login
+        username_type = self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input")
+        username_type.send_keys(username)
+        password_type = self.driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input")
+        password_type.send_keys(password)
+        submit = self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]/button')
+        submit.click()
+
+
+
+my_bot = InstaUnfollowers(username, password)
+my_bot.get_unfollowers()
+try:
+    my_bot.driver.close()
+except:
+    print("Fail")
+    my_bot.driver.close()
